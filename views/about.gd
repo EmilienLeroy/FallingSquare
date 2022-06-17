99163fc63084d330a27dd625cf3dd3faa0a2a3f9
Emilien Leroy <emilien1005@hotmail.fr>
2022-06-17 19:47:52 +0200

    🔧 Add version config file

--- views/about.gd
+++ views/about.gd
@@ -3,9 +3,19 @@ extends Node2D
 signal back;
 
 func _ready():
+	$Version.text = "Version " + get_version();
 	$Back.connect("button_down", self, "_on_back_down");
 	pass 
 
 func _on_back_down():
 	emit_signal("back");
 
+
+func get_version():
+	var config = ConfigFile.new();	
+	var err = config.load("res://version.cfg");
+	
+	if err != OK:
+		return
+		
+	return config.get_value(config.get_sections()[0], "version");
