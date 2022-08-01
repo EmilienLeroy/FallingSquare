190b026e64b77311530325f2c3f77d11febb87bc
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-02 00:17:24 +0200

    💄 Update score and combo style

--- components/combo.gd
+++ components/combo.gd
@@ -2,9 +2,10 @@ extends CanvasLayer
 
 var combo = 0 setget set_combo, get_combo;
 
+
 func set_combo(c): 
 	combo = c;
-	$HBoxContainer/Value.text = 'x' + str(combo);
+	$Panel/HBoxContainer/Value.text = 'x' + str(combo);
 
 func get_combo():
 	return combo;
--- components/score.gd
+++ components/score.gd
@@ -4,7 +4,7 @@ var score = 0 setget set_score, get_score;
 
 func set_score(s): 
 	score = s;
-	$Value.text = str(score);
+	$HBoxContainer/Value.text = str(score);
 
 func get_score():
 	return score;
