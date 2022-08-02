5215723960b0b121fee47ba712e5246666aae289
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-02 18:59:21 +0200

    ✨ Animate combo in and out

--- components/combo.gd
+++ components/combo.gd
@@ -4,6 +4,12 @@ var combo = 0 setget set_combo, get_combo;
 
 
 func set_combo(c): 
+	if (c > 0 && combo == 0):
+		$AnimationPlayer.play("display");
+		
+	if (c == 0 && combo > 0):
+		$AnimationPlayer.play("hide");
+
 	combo = c;
 	$Panel/HBoxContainer/Value.text = 'x' + str(combo);
 
