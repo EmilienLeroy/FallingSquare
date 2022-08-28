625e8dac532ac080c94b9afa78899a9a0f041b6f
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-28 23:09:03 +0200

    🔧 Add highscore variable on project setting

--- views/save.gd
+++ views/save.gd
@@ -3,10 +3,22 @@ extends Node2D
 signal cancel;
 
 var score = 0;
+var highscore_url = ProjectSettings.get_setting("application/config/highscore");
 
 func _ready():
+	$Send.connect("button_down", self, "_on_send");
 	$Cancel.connect("button_down", self, "_on_cancel");
 	$Value.text = str(score);
+	
+	if (!highscore_url):
+		# TODO: display an error
+		return;
+	
+	# TODO: get score from highscore
+
+func _on_send():
+	# TODO: send score to highscore 
+	pass
 
 func _on_cancel():
 	emit_signal("cancel", score);
