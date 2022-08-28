c9d7ad95b7edcdee92f23f6c5f2633dac47bc5f6
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-28 23:31:18 +0200

    🚧 Get score from an highscore instance

--- views/save.gd
+++ views/save.gd
@@ -8,13 +8,19 @@ var highscore_url = ProjectSettings.get_setting("application/config/highscore");
 func _ready():
 	$Send.connect("button_down", self, "_on_send");
 	$Cancel.connect("button_down", self, "_on_cancel");
+	$GET.connect("request_completed", self, "_on_get_scores");
+	
 	$Value.text = str(score);
 	
 	if (!highscore_url):
 		# TODO: display an error
 		return;
 	
-	# TODO: get score from highscore
+	var error = $GET.request(highscore_url);
+
+	if (error != OK):
+		# TODO: display an error
+		return;
 
 func _on_send():
 	# TODO: send score to highscore 
@@ -22,3 +28,9 @@ func _on_send():
 
 func _on_cancel():
 	emit_signal("cancel", score);
+	
+func _on_get_scores(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	var ranks = json.result;
+	
+	# TODO: Display scores
