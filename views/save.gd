0118c17059228e358bbfab5eec25d808b02e2323
Emilien Leroy <emilien1005@hotmail.fr>
2022-09-04 22:19:46 +0200

    ♻️ Move all highscore http request in one module file

--- views/save.gd
+++ views/save.gd
@@ -4,26 +4,26 @@ signal cancel;
 signal send;
 
 var Rank = preload("res://components/rank.tscn");
+var Highscore = preload("res://modules/highscore.gd");
 
 var score = 0;
 var highscore_url = ProjectSettings.get_setting("application/config/highscore");
+var highscore = Highscore.new(highscore_url);
 
 func _ready():
+	add_child(highscore);
+
 	$Send.connect("button_down", self, "_on_send");
 	$Cancel.connect("button_down", self, "_on_cancel");
-	$GET.connect("request_completed", self, "_on_get_scores");
-	
 	$Value.text = str(score);
 	
-	if (!highscore_url):
+	var result = yield(highscore.get_scores(4), 'completed');
+	
+	if (result.err != OK):
 		# TODO: display an error
 		return;
 	
-	var error = $GET.request(highscore_url + "?limit=4");
-
-	if (error != OK):
-		# TODO: display an error
-		return;
+	add_scores(result.data);
 
 func _on_send():
 	var name = $Name.text;
@@ -32,66 +32,19 @@ func _on_send():
 		# TODO: display an error
 		return;
 
-	var query = JSON.print({
-		"name": name,
-		"value": score,
-	});
+	var result = yield(highscore.post_score(name, score), 'completed');
 	
-	var headers = [
-		"Content-Type: application/json",
-		"Cookie: " + read_cookies()
-	];
-	
-	var err = $POST.request(highscore_url, headers, true, HTTPClient.METHOD_POST, query);
-	var result = yield($POST, "request_completed");
-	var json = JSON.parse(result[3].get_string_from_utf8()).result;
-	var cookies = get_cookie_session(result[2]); 
-	
-	if (cookies):
-		save_cookies(cookies);
-
-	if (err or !json):
-		# TODO: display error
+	if (result.err != OK):
+		# TODO: display an error
 		return;
-
+	
 	emit_signal("send");
 	pass
-
-func get_cookie_session(headers):
-	var session = '';
-	
-	for h in headers:
-		if h.to_lower().begins_with("set-cookie"):
-			session = h.split(":", true, 1)[1].strip_edges();
-			
-	return session;
-
-func save_cookies(c):
-	var file = File.new();
-
-	file.open("user://data.save", File.WRITE);
-	file.store_string(var2str({ "cookies": c }));
-	file.close();
-	
-func read_cookies():
-	var file = File.new()
-	
-	file.open("user://data.save", File.READ);
-	var data = str2var(file.get_as_text());
-	file.close();
-	
-	if (data):
-		return data["cookies"];
-	
-	return ''; 
-
 
 func _on_cancel():
 	emit_signal("cancel", score);
 	
-func _on_get_scores(result, response_code, headers, body):
-	var ranks = JSON.parse(body.get_string_from_utf8()).result;
-	
+func add_scores(ranks):
 	if (!ranks):
 		# TODO: display error
 		return;
