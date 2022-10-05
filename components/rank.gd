e773269032d8557e46d788d337fc3b41b2cd8bb2
Emilien Leroy <emilien1005@hotmail.fr>
2022-10-05 07:31:54 +0200

    💄 Improve style of rank component

--- components/rank.gd
+++ components/rank.gd
@@ -1,7 +1,7 @@
 extends MarginContainer
 
 func set_rank(rank):
-	$HBoxContainer/Rank.text = str(rank.rank) + ".";
-	$HBoxContainer/Name.text = str(rank.name);
-	$HBoxContainer/Value.text = str(rank.value);
+	$Panel/HBoxContainer/Rank.text = str(rank.rank) + ".";
+	$Panel/HBoxContainer/Name.text = str(rank.name);
+	$Panel/HBoxContainer/Value.text = str(rank.value);
 
