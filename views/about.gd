51a63ea79a031a72dc298bb2015cb33576d45fe6
Emilien Leroy <emilien1005@hotmail.fr>
2022-10-07 07:20:18 +0200

    💄 Add version on home view and add links on about view

--- views/about.gd
+++ views/about.gd
@@ -3,13 +3,16 @@ extends Node2D
 signal back;
 
 func _ready():
-	$Version.text = "Version " + get_version();
 	$Back.connect("button_down", self, "_on_back_down");
+	$Github.connect("button_down", self, "_on_github_down");
+	$Icons8.connect("button_down", self, "_on_icons8_down");
 	pass 
 
 func _on_back_down():
 	emit_signal("back");
 
+func _on_github_down():
+	OS.shell_open('https://github.com/EmilienLeroy/FallingSquare');
 
-func get_version():
-	return ProjectSettings.get_setting("application/config/version");
+func _on_icons8_down():
+	OS.shell_open('https://icons8.com');
